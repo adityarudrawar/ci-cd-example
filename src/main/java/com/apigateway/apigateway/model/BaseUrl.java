68256700eea12e3313bf264c3d8e006291d511ac
Aditya Arun Rudrawar <arudrawa@iu.edu>
2022-04-03 13:15:53 -0400

    Gateway Added

---
+++ src/main/java/com/apigateway/apigateway/model/BaseUrl.java
@@ -0,0 +1,25 @@
+package com.apigateway.apigateway.model;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BaseUrl {
+    public String Registry = "http://localhost:7072";
+    //    public String Logger = "http://localhost:8080";
+    public String Plotting = "http://localhost:5000";
+
+
+
+//    public String Plotting = "http://4a99-2001-18e8-2-28b6-e860-f7c1-1087-b608.ngrok.io";
+    public String Logger = "http://ddc3-2001-18e8-2-28b6-fdaa-7b54-33a1-8be.ngrok.io";
+//    public String Registry = "http://ddc3-2001-18e8-2-28b6-fdaa-7b54-33a1-8be.ngrok.io";
+
+
+
+//  Registry on Tanu's Registry
+//    public String Registry = "http://e4a1-66-244-80-73.ngrok.io";
+
+//    Registry on Parnals side.
+//    public String Registry = "http://e263-2001-18e8-2-28b8-f000-00-dcc.ngrok.io";
+}
+
